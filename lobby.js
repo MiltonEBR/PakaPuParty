38d82a51304945e98e7c93921cf02b55b54ec945
Milton <A01634505@itesm.mx>
2020-10-27 14:08:17 -0600

    Create and join forms working

--- lobby.js
+++ lobby.js
@@ -4,10 +4,38 @@ const createBtn=document.querySelector('#create');
 const form=document.querySelector('#lobby-form');
 const backBtn=form.querySelector('#back');
 
-const showForm=function(){
+const playerInput=document.querySelector('#players');
+const roomInput=document.querySelector('#room');
+const passInput=document.querySelector('#password');
+
+const joinForm=function(){
+
     if(form.classList.contains('hidden')){
         form.classList.remove('hidden');
         form.classList.add('visible');
+    }
+
+    if(roomInput.classList.contains('disabled')){
+        roomInput.classList.remove('disabled');
+    }
+
+    if(!playerInput.classList.contains('disabled')){
+        playerInput.classList.add('disabled');
+    }
+}
+
+const createForm=function(){
+    if(form.classList.contains('hidden')){
+        form.classList.remove('hidden');
+        form.classList.add('visible');
+    }
+
+    if(!roomInput.classList.contains('disabled')){
+        roomInput.classList.add('disabled');
+    }
+
+    if(playerInput.classList.contains('disabled')){
+        playerInput.classList.remove('disabled');
     }
 }
 
@@ -18,9 +46,11 @@ const hideForm=function(){
     }
 }
 
-joinBtn.addEventListener('click',showForm);
-createBtn.addEventListener('click',showForm);
+joinBtn.addEventListener('click',joinForm);
+createBtn.addEventListener('click',createForm);
 
 backBtn.addEventListener('click',(e)=>{
     e.preventDefault();
+    roomInput.value='';
+    passInput.value='';
     hideForm()});

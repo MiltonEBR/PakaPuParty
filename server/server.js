4565a509aebb9adc5b6dfb10ab7eca4656b71281
Milton <A01634505@itesm.mx>
2020-11-02 18:02:26 -0600

    Now serves static files from a fake directory

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const fs=require('fs');
+
 const socketio=require('socket.io');
 
 const app=express();
 
-app.use(express.static(`${__dirname}/../client`));
+app.use('/public',express.static(`${__dirname}/../client`));
+
+app.get('/',(req,res)=>{
+    res.sendFile('index.html',{root:`../client`});
+});
+
+app.get('/play',(req,res)=>{
+    res.sendFile('lobby.html',{root:`../client`})
+})
 
 const server= app.listen(3000,()=>{
     console.log('listening')
 });
 
 const io=socketio(server);
+

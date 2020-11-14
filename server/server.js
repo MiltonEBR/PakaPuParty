7c884ed6ca45ad1a2f0e09e4a57767c44baf97ef
Milton <A01634505@itesm.mx>
2020-11-14 11:14:34 -0600

    Correctly receiving create req and giving a response

--- server/server.js
+++ server/server.js
@@ -5,17 +5,20 @@ const socketio = require('socket.io');
 const app = express();
 app.use('/public', express.static(`${__dirname}/../client`));
 app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: `../client` });
 });
 
 app.get('/play', (req, res) => {
+    console.log(req.query);
     res.sendFile('lobby.html', { root: `../client` });
 });
 
 app.post('/play', (req, res) => {
     console.log(req.body);
+    res.send('numero');
 });
 
 const server = app.listen(3000, () => {

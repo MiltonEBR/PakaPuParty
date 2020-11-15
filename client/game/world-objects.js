e0d8820e93ffb41b8730e8597300c5eef663b047
Milton <A01634505@itesm.mx>
2020-11-15 17:01:13 -0600

    Initial commit of game objects creator

---
+++ client/game/world-objects.js
@@ -0,0 +1,19 @@
+class WorldObjects {
+    createPlayer(x, y) {
+        const player = Bodies.rectangle(x, y, 50, 50);
+        player.render.lineWidth = 4;
+        player.render.strokeStyle = 'blue';
+        return player;
+    }
+
+    createTile(x, y) {
+        const tile = Bodies.rectangle(x, y, 100, 100);
+        tile.render.lineWidth = 2;
+        tile.render.strokeStyle = 'gray';
+        tile.draw = (ctx) => {
+            ctx.fillStyle = 'green';
+            ctx.fillRect(tile.position.x - 25, tile.position.y - 25, 50, 50);
+        };
+        return tile;
+    }
+}

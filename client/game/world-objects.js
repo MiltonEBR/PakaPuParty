4a555c213d504e207f7ffb156384917990c18d8d
Milton <A01634505@itesm.mx>
2020-11-16 11:28:52 -0600

    Made filters a public object via getter

--- client/game/world-objects.js
+++ client/game/world-objects.js
@@ -1,19 +1,49 @@
 class WorldObjects {
+    constructor() {
+        this._filterList = {
+            notInteractable: 0x0001,
+            interactable: 0x0002,
+        };
+    }
+
+    get filterList() {
+        return this._filterList;
+    }
+
     createPlayer(x, y) {
-        const player = Bodies.rectangle(x, y, 50, 50);
+        const player = Bodies.rectangle(x, y, 50, 50, {
+            collisionFilter: {
+                category: this._filterList.notInteractable,
+            },
+        });
         player.render.lineWidth = 4;
         player.render.strokeStyle = 'blue';
         return player;
     }
 
-    createTile(x, y) {
+    createTile(x, y, towards) {
         const tile = Bodies.rectangle(x, y, 100, 100, { isStatic: true });
         tile.isSensor = true;
         tile.render.lineWidth = 2;
         tile.render.strokeStyle = 'gray';
         tile.draw = (ctx) => {
-            ctx.fillStyle = 'green';
-            ctx.fillRect(tile.position.x - 25, tile.position.y - 25, 50, 50);
+            switch (towards) {
+                case 'null':
+                    ctx.fillStyle = 'green';
+                    ctx.fillRect(tile.position.x - 25, tile.position.y - 25, 50, 50);
+                    break;
+                case 'up':
+                    ctx.fillStyle = 'green';
+                    ctx.fillRect(tile.position.x - 25, tile.position.y - 50, 50, 100);
+                    break;
+                case 'side':
+                    ctx.fillStyle = 'green';
+                    ctx.fillRect(tile.position.x - 50, tile.position.y - 25, 100, 50);
+                    break;
+
+                default:
+                    break;
+            }
         };
         return tile;
     }

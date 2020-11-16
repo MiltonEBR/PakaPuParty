b87e189f684654027115ec9e9797290736f3ef22
Milton <A01634505@itesm.mx>
2020-11-16 11:28:28 -0600

    Mouse filtering working

--- client/game/game.js
+++ client/game/game.js
@@ -17,13 +17,17 @@ function initGame() {
     const objects = new WorldObjects();
 
     const player = objects.createPlayer(200, 200);
-    const tile = objects.createTile(400, 200);
-
+    const tiles = [
+        objects.createTile(400, 200, 'up'),
+        objects.createTile(500, 200, 'side'),
+        objects.createTile(600, 200, 'null'),
+    ];
     const mouse = Mouse.create(gameCanvas);
     const mouseConstraint = MouseConstraint.create(engine, {
         mouse: mouse,
+        collisionFilter: { mask: objects.filterList.interactable },
     });
-    World.add(engine.world, [tile, player, mouseConstraint]);
+    World.add(engine.world, [...tiles, player, mouseConstraint]);
 
     renderer.run();
     engine.world.gravity.y = 0;

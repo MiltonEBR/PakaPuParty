3c445ed0827a0efdf21b57fb140bf892e938517c
Milton <A01634505@itesm.mx>
2020-11-21 19:27:27 -0600

    Manages new world objects

--- client/game/renderer.js
+++ client/game/renderer.js
@@ -1,6 +1,6 @@
 class Renderer {
-    constructor({ bodies }, canvas, options) {
-        this.bodies = bodies;
+    constructor({ entityList }, canvas, options) {
+        this.entityList = entityList;
         this.canvas = canvas;
         this.ctx = canvas.getContext('2d');
 
@@ -14,8 +14,8 @@ class Renderer {
     draw() {
         const ctx = this.ctx;
         const options = this.options;
-
-        this.bodies.forEach((body) => {
+        for (let entity in this.entityList) {
+            const body = this.entityList[entity];
             if (body.render.draw) {
                 body.render.draw(ctx);
             }
@@ -51,7 +51,44 @@ class Renderer {
 
                 ctx.stroke();
             }
-        });
+        }
+        // this.entityList.forEach((body) => {
+        //     if (body.render.draw) {
+        //         body.render.draw(ctx);
+        //     }
+
+        //     let optionsWireframe =
+        //         options.wireframes &&
+        //         (body.render.wireframe || body.render.wireframe === undefined);
+
+        //     if (optionsWireframe || body.render.wireframe) {
+        //         ctx.beginPath();
+        //         ctx.moveTo(body.vertices[0].x, body.vertices[0].y);
+
+        //         for (var j = 1; j < body.vertices.length; j += 1) {
+        //             ctx.lineTo(body.vertices[j].x, body.vertices[j].y);
+        //         }
+        //         ctx.closePath();
+        //         ctx.lineWidth = 1;
+        //         ctx.strokeStyle = 'black';
+        //         if (body.render) {
+        //             if (body.render.lineWidth) {
+        //                 ctx.lineWidth = body.render.lineWidth;
+        //             }
+
+        //             if (body.render.strokeStyle) {
+        //                 ctx.strokeStyle = body.render.strokeStyle;
+        //             }
+        //         }
+        //         //Fill wireframes, but kind of useless so it's commented out for now
+        //         //               if(body.render.fillStyle){
+        //         //                   ctx.fillStyle=body.render.fillStyle;
+        //         //                    ctx.fill();
+        //         //                }
+
+        //         ctx.stroke();
+        //     }
+        // });
     }
 
     run() {

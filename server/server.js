8c479cc674b790acc57b5ab35ec5230779144b50
Milton <A01634505@itesm.mx>
2020-11-25 13:44:21 -0600

    Refactoring game into 1 page

--- server/server.js
+++ server/server.js
@@ -5,27 +5,9 @@ const Game = require('./game/game');
 const app = express();
 
 app.use('/public', express.static(`${__dirname}/../client`));
-app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: `../client` });
-});
-
-app.get('/play', (req, res) => {
-    console.log(req.query);
-    res.sendFile('game.html', { root: `../client` });
-});
-
-app.post('/play', (req, res) => {
-    console.log(req.body);
-    let room;
-    if (req.body.room) {
-        room = 'join';
-    } else if (req.body.private) {
-        room = 'create';
-    }
-    res.send(room);
 });
 
 const server = app.listen(3000, () => {
@@ -33,16 +15,30 @@ const server = app.listen(3000, () => {
 });
 
 const io = socketio(server);
-const game = new Game();
+
+// const clientRooms = {};
+// const games = {};
+
+// const handleCreate = (name, priv) => {
+//     let roomName = makeid(5);
+//     games[roomName] = new Game();
+//     return roomName;
+// };
+
+// const handleJoin = (name, room) => {
+//     if (games[room]) {
+//     }
+// };
+
+// const game = new Game();
 // game.createBox({ x: 245, y: 160 });
 io.on('connection', (client) => {
-    game.createPlayer();
-    const serializedData = game.serializeAll();
-    client.emit('init', serializedData);
-    console.log('someone conected');
-
-    setInterval(function () {
-        let message = game.serializeAll().players;
-        client.emit('update', message);
-    }, 20);
+    // game.createPlayer();
+    // const serializedData = game.serializeAll();
+    // client.emit('init', serializedData);
+    // console.log('someone conected');
+    // setInterval(function () {
+    //     let message = game.serializeAll().players;
+    //     client.emit('update', message);
+    // }, 20);
 });

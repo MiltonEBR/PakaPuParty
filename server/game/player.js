8ebb617be8affab6b7a1d68d638cf63fd8378cfb
Milton <A01634505@itesm.mx>
2020-12-11 14:15:52 -0600

    Now has reference to socket, new variables for movement

--- server/game/player.js
+++ server/game/player.js
@@ -1,20 +1,26 @@
 const Matter = require('matter-js');
 
 class Player {
-    constructor(name, spawnTile, game) {
+    constructor(name, spawnTile, game, sock) {
         this._currentTile = spawnTile;
         this._game = game;
         this._name = name;
         this._instance;
         //this._items;
-        //this._socket;
+        this._socket = sock;
         this._points = 0;
         this._color = '';
+        this.movesLeft = 0;
 
         this.spawn(spawnTile.position);
     }
 
-    move() {
+    turn() {
+        this._socket.emit('yourTurn');
+    }
+
+    move(num) {
+        this.movesLeft = num;
         Matter.Body.setVelocity(this._instance, { x: 1, y: 0 });
     }
 
@@ -42,6 +48,11 @@ class Player {
         this._points += points;
     }
 
+    tileCollision() {
+        this.movesLeft = Math.max(Math.min(this.movesLeft - 1, Math.max(0, 6)), Math.min(0, 6));
+        return this.movesLeft;
+    }
+
     spawn(position) {
         const { x, y } = position;
         const playerSize = 50;
@@ -54,6 +65,7 @@ class Player {
                 mask: this._game.filterList.world,
             },
         });
+        this._instance.label = 'player';
     }
 
     serialize() {

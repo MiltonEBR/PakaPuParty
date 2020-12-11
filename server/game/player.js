5b804d8d32b74fa2b4a75bfa1fcf0b3c498087da
Milton <A01634505@itesm.mx>
2020-12-11 14:58:11 -0600

    Player can now move between tiles

--- server/game/player.js
+++ server/game/player.js
@@ -1,3 +1,4 @@
+const { Vector } = require('matter-js');
 const Matter = require('matter-js');
 
 class Player {
@@ -11,6 +12,7 @@ class Player {
         this._points = 0;
         this._color = '';
         this.movesLeft = 0;
+        this.speed = 1;
 
         this.spawn(spawnTile.position);
     }
@@ -19,9 +21,31 @@ class Player {
         this._socket.emit('yourTurn');
     }
 
-    move(num) {
-        this.movesLeft = num;
-        Matter.Body.setVelocity(this._instance, { x: 1, y: 0 });
+    moveTiles(num) {
+        if (num) {
+            this.movesLeft = num;
+        }
+        const availableTiles = this._currentTile.getWalkableTiles();
+        let availableKeys = Object.keys(availableTiles);
+        if (availableKeys.length === 1) {
+            this.moveTo(availableTiles[availableKeys[0]].position);
+        } else {
+            this._instance.frictionAir = 0.05;
+            sock.emit('selectDirection', { options: availableKeys });
+        }
+    }
+
+    moveTo({ x, y }) {
+        this._instance.frictionAir = 0.0;
+        const targetVector = Matter.Vector.create(x, y),
+            playerVector = Matter.Vector.create(
+                this._instance.position.x,
+                this._instance.position.y
+            );
+
+        const dirVector = Matter.Vector.normalise(Vector.sub(targetVector, playerVector));
+
+        Matter.Body.setVelocity(this._instance, Vector.mult(dirVector, this.speed));
     }
 
     get color() {
@@ -48,8 +72,15 @@ class Player {
         this._points += points;
     }
 
-    tileCollision() {
+    tileCollision(tile) {
+        this._currentTile = tile;
         this.movesLeft = Math.max(Math.min(this.movesLeft - 1, Math.max(0, 6)), Math.min(0, 6));
+        if (this.movesLeft < 1) {
+            this._instance.frictionAir = 0.05;
+        } else {
+            this.moveTiles();
+        }
+
         return this.movesLeft;
     }
 

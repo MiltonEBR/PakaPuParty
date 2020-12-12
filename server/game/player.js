e664018d275fe5b7e08dc747741473e24c648206
Milton <A01634505@itesm.mx>
2020-12-12 01:18:54 -0600

    Handles player movement with multiple paths

--- server/game/player.js
+++ server/game/player.js
@@ -12,7 +12,7 @@ class Player {
         this._points = 0;
         this._color = '';
         this.movesLeft = 0;
-        this.speed = 1;
+        this.speed = 2;
 
         this.spawn(spawnTile.position);
     }
@@ -21,14 +21,22 @@ class Player {
         this._socket.emit('yourTurn');
     }
 
-    moveTiles(num) {
-        if (num) {
-            this.movesLeft = num;
+    setMoves(num) {
+        this.movesLeft = num;
+    }
+
+    moveTiles(dir) {
+        if (this.movesLeft <= 0) {
+            return;
         }
         const availableTiles = this._currentTile.getWalkableTiles();
         let availableKeys = Object.keys(availableTiles);
+
         if (availableKeys.length === 1) {
             this.moveTo(availableTiles[availableKeys[0]].position);
+        } else if (dir && this._currentTile[dir].canMove) {
+            const nextTile = this._currentTile[dir].tile;
+            this.moveTo(nextTile.position);
         } else {
             this._instance.frictionAir = 0.05;
             this._socket.emit('selectDirection', { options: availableKeys });
@@ -68,13 +76,26 @@ class Player {
         return this._points;
     }
 
+    get socket() {
+        return this._socket;
+    }
+
     set addPoints(points) {
         this._points += points;
     }
 
     tileCollision(tile) {
         this._currentTile = tile;
+        if (this.movesLeft - 1 >= 0) {
+            this._game.emitToPlayers('preview', {
+                item: 'diceLeft',
+                val: this.movesLeft - 1,
+                username: this._name,
+            });
+        }
+
         this.movesLeft = Math.max(Math.min(this.movesLeft - 1, Math.max(0, 6)), Math.min(0, 6));
+
         if (this.movesLeft < 1) {
             this._instance.frictionAir = 0.05;
         } else {
@@ -89,6 +110,7 @@ class Player {
         const playerSize = 50;
         this._instance = this._game.createInstance(x, y, playerSize, playerSize, {
             frictionAir: 0.05,
+            inertia: Infinity,
             // restitution: 0.0,
             // density: 1,
             collisionFilter: {
